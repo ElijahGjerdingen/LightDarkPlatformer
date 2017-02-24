9826dbe89991a8f87b01e78a37ae0c099615d0cd
Elijah Gjerdingen <elijah.gjerdingen@gmail.com>
2017-02-23 21:07:05 -0600

    Created Preload and shuffled locations of script loads
    Created Preload for scene script and shuffled locations of script loads

--- Scene/SceneScript.js
+++ Scene/SceneScript.js
@@ -1,1 +1,28 @@
 // JavaScript source code
+var stage;
+var meter = 100;
+
+function load()
+{
+    preload = new createjs.loadQueue(true);
+    preload.installPlugin(createjs.Sound);
+    createjs.sound.alternateExtensions = ["ogg"]; // <- switch this ti alternate musical file type
+    preload.addEventListner("complete", init);
+
+    preload.loadManifest([
+        { id: "LBackground", src: "/scene/LBackground.png" },
+        { id: "DBackground", src: "/scene/DBackground.png"},
+        { id: "LGround", src: "/scene/LGround.png" },
+        { id: "DGround", src: "/scene/DGround.png"},
+        { id: "Lamp", src: "/scene/Lamp.png" },
+        { id: "LMusic", src: "/scene/LMusic.png" },
+        { id: "DMusic", src: "/scene/DMusic.png"}
+    ])
+    preload.Load();
+}
+
+function init()
+{
+    stage = new createjs.Stage("canvas");
+
+}
