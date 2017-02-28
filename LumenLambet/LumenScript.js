2ad8d0eeb01775ba823181d8ab6d77e7c638ed05
Elijah Gjerdingen <elijah.gjerdingen@gmail.com>
2017-02-27 23:39:19 -0600

    Added framework for player movement
    Added framework for player movement into the player script.

--- LumenLambet/LumenScript.js
+++ LumenLambet/LumenScript.js
@@ -1,1 +1,23 @@
-// JavaScript source code
+//Lumen code
+const ARROW_KEY_Up = 56;
+const ARROW_KEY_Down = 53;
+const ARROW_KEY_Left = 54;
+const ARROW_KEY_Right = 55;
+
+var leftkeydown = false;
+var rightkeydown = false;
+var upkeydown = false;
+var downkeydown = false;
+
+function load()
+{
+    preload = new createjs.loadQueue(true);
+    preload.installPlugin(createjs.Sound);
+    createjs.Sound.alternateExtensions = ["ogg"];
+    preload.addEventListener("complete", init);
+
+    preload.loadManifest([
+        { id: "PlayerSprite", src: "/LumenLambent/Lumen" },
+        { id: "", src: ""}
+    ]);
+}
