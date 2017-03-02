f330167ab3b1bb612fd168a0f5c0c400fe89c47b
Elijah Gjerdingen <elijah.gjerdingen@gmail.com>
2017-03-02 00:21:50 -0600

    Paralaxing looping background BITCHES!
    Paralaxing looping background BITCHES!

--- Scene/SceneScript.js
+++ Scene/SceneScript.js
@@ -4,6 +4,10 @@ var meter = 100;
 var platform = [];
 var lumenPlaceholder;
 var backgroundContainer;
+var darkBackgroundContainer;
+var backgroundContainer2;
+var darkBackgroundContainer2;
+var testText = new createjs.Text("test", "100px Ariel", "black");
 function load()
 {
     preload = new createjs.LoadQueue(true);
@@ -13,17 +17,20 @@ function load()
     preload.loadManifest([
         { id: "LBackground", src: "/Scene/Sky.png" },
         { id: "Mountains", src: "/Scene/Backhill.png" },
-        { id: "Trees", src: "/Scene/Trees.png"},
-        /*{ id: "DBackground", src: "/DBackground.png"},
-        { id: "LGround", src: "/LGround.png" },
-        { id: "DGround", src: "/DGround.png"},
-        { id: "Lamp", src: "/Lamp.png" },
+        { id: "Trees", src: "/Scene/Trees.png" },
+        { id: "DBackground", src: "/Scene/SkyDark.png" },
+        { id: "LGround", src: "/Scene/GroundLayer.png" },
+        { id: "DGround", src: "/Scene/GroundLayerDark.png" },
+        /*{ id: "Lamp", src: "/Lamp.png" },
         { id: "LMusic", src: "/LMusic.png" },
         { id: "DMusic", src: "/DMusic.png" },*/
         { id: "SLPlatform", src: "/Scene/ShortFloatPlatform.png" },
         { id: "LLPlatform", src: "/Scene/LongFloatPlatform.png" },
-        { id: "LGround", src: "/Scene/GroundLayer.png"}
-    ])
+        { id: "SDPlatform", src: "/Scene/ShortFloatDark.png" },
+        { id: "LDPlatform", src: "/Scene/LongFloatDark.png" },
+        { id: "DarkTrees", src: "/Scene/TreesDark.png" },
+        { id: "DarkMountains", src: "/Scene/BackhillDark.png" }
+    ]);
     preload.load();
 }
 
@@ -37,11 +44,36 @@ function init()
     var background = new createjs.Bitmap(preload.getResult("LBackground"));
     var mountains = new createjs.Bitmap(preload.getResult("Mountains"));
     var trees = new createjs.Bitmap(preload.getResult("Trees"));
-    
-    backgroundContainer = new createjs.Container();
-    backgroundContainer.addChild(background, mountains, trees);
+    background.x = 0;
+    mountains.x = 0;
+    trees.x = 0;
+    var background2 = new createjs.Bitmap(preload.getResult("LBackground"));
+    var mountains2 = new createjs.Bitmap(preload.getResult("Mountains"));
+    var trees2 = new createjs.Bitmap(preload.getResult("Trees"));
+    background2.x = 1900;
+    mountains2.x = 1900;
+    trees2.x = 1900;
 
-    stage.addChild(backgroundContainer);
+    var darkBackground = new createjs.Bitmap(preload.getResult("DBackground"));
+    var darkMountains = new createjs.Bitmap(preload.getResult("DarkMountains"));
+    var darkTrees = new createjs.Bitmap(preload.getResult("DarkTrees"));
+    darkBackground.x = 0;
+    darkMountains.x = 0;
+    darkTrees.x = 0;
+    var darkBackground2 = new createjs.Bitmap(preload.getResult("DBackground"));
+    var darkMountains2 = new createjs.Bitmap(preload.getResult("DarkMountains"));
+    var darkTrees2 = new createjs.Bitmap(preload.getResult("DarkTrees"));
+    darkBackground2.x = 1900;
+    darkMountains2.x = 1900;
+    darkTrees2.x = 1900;
+
+    backgroundContainer = new createjs.Container();
+    backgroundContainer.addChild(background, background2, mountains, mountains2, trees, trees2);
+    darkBackgroundContainer = new createjs.Container();
+    darkBackgroundContainer.addChild(darkBackground, darkBackground2, darkMountains, darkMountains2, darkTrees, darkTrees2);
+
+    stage.addChild(backgroundContainer, darkBackgroundContainer, testText);
+    darkBackgroundContainer.visible = false;
 
     createjs.Sound.play("LMusic", createjs.Sound.INTERUPT_NONE, 0, 0, -1, .5, 0);
     createBlocks();
@@ -56,9 +88,10 @@ function createBlocks()
     platform[0].x = x;
     platform[0].y = y;
     stage.addChild(platform[0]);
-    platform.push( new createjs.Bitmap(preload.getResult("LGround")));
-    platform[1].x = x + 150;
+    platform.push( new createjs.Bitmap(preload.getResult("DGround")));
+    platform[1].x = x;
     platform[1].y = y;
+    platform[1].visible = false;
     stage.addChild(platform[1]);
 }
 function CheckRectIntersection( platform, character)
@@ -74,12 +107,49 @@ function CheckRectIntersection( platform, character)
 }
 function moveScene()
 {
-    backgroundContainer.getChildByName("background").x += 1;
-    backgroundContainer.getChildByName("mountains").x += 2;
-    backgroundContainer.getChildByName("trees").x += 3;
+    for (var i = 0; i < 3; i++)
+    {
+        backgroundContainer.getChildAt((2 * i)).x -= (i + 1);
+        backgroundContainer.getChildAt((2 * i) + 1).x -= (i + 1);
+        darkBackgroundContainer.getChildAt((2 * i)).x -= (i + 1);
+        darkBackgroundContainer.getChildAt((2 * i) + 1).x -= (i + 1);
+        if (backgroundContainer.getChildAt(2 * i).x + backgroundContainer.getChildAt(2 * i).image.width <= 0) {
+            backgroundContainer.getChildAt(2 * i).x = 1900;
+            darkBackgroundContainer.getChildAt(2 * i).x = 1900;
+        }
+        if (backgroundContainer.getChildAt((2 * i) + 1).x + backgroundContainer.getChildAt((2 * i) + 1).image.width <= 0) {
+            backgroundContainer.getChildAt((2 * i) + 1).x = 1900;
+            darkBackgroundContainer.getChildAt((2 * i) + 1).x = 1900;
+        }
+    }
+    testText.text = "";
+    for(var i = 0; i < 6; i++)
+    {
+        testText.text += backgroundContainer.getChildAt(i).x + " ";
+    }
+    
 }
 function tick()
 {
+    if (meter <= 50)
+    {
+        backgroundContainer.visible = false;
+        darkBackgroundContainer.visible = true;
+        platform[0].visible = false;
+        platform[1].visible = true;
+    }
+    else if (meter > 50)
+    {
+        meter -= 1/15;
+    }
+    else if (meter == 100)
+    {
+        backgroundContainer.visible = true;
+        darkBackgroundContainer.visible = false;
+        platform[0].visible = true;
+        platform[1].visible = false;
+    }
     moveScene();
+    
     stage.update();
 }
