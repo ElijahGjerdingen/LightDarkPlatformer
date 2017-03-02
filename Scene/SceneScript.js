f92ce404318416ba76a5ca329ce516d464d51059
Elijah Gjerdingen <elijah.gjerdingen@gmail.com>
2017-03-01 18:49:39 -0600

    Got Background and floor working
    Got the Background and floor working with correct pathing.

--- Scene/SceneScript.js
+++ Scene/SceneScript.js
@@ -9,14 +9,14 @@ function load()
     preload.addEventListener("complete", init);
 
     preload.loadManifest([
-        { id: "LBackground", src: "LightBackground.png" },
-        { id: "DBackground", src: "/DBackground.png"},
+        { id: "LBackground", src: "/Scene/LightBackground.png" },
+        /*{ id: "DBackground", src: "/DBackground.png"},
         { id: "LGround", src: "/LGround.png" },
         { id: "DGround", src: "/DGround.png"},
         { id: "Lamp", src: "/Lamp.png" },
         { id: "LMusic", src: "/LMusic.png" },
-        { id: "DMusic", src: "/DMusic.png" },
-        { id: "placeHolder", src: "/placeHolder.png"}
+        { id: "DMusic", src: "/DMusic.png" },*/
+        { id: "placeHolder", src: "/Scene/placeHolder.png"}
     ])
     preload.load();
 }
@@ -25,8 +25,8 @@ function init()
 {
     stage = new createjs.Stage("canvas");
 
-    createjs.Ticker.setfps(60);
-    createjs.Ticker.addEventListner("tick", tick);
+    createjs.Ticker.setFPS(60);
+    createjs.Ticker.addEventListener("tick", tick);
 
     var background = new createjs.Bitmap(preload.getResult("LBackground"));
     background.setTransform(0, 0, 1, 1);
@@ -39,10 +39,18 @@ function init()
 function createBlocks()
 {
     //var block1 = new createjs.Shape(new createjs.Graphics().beginFill("green").drawRect(100, 100, 50, 200));
-    var x = 100;
-    var y = 100;
-    var platform = new Bitmap(preload.getResult("placeHolder"));
+    var x = 10;
+    var y = 800;
+    var platform = new createjs.Bitmap(preload.getResult("placeHolder"));
     platform.x = x;
     platform.y = y;
     stage.addChild(platform);
+    var platform2 = new createjs.Bitmap(preload.getResult("placeHolder"));
+    platform2.x = x + 400;
+    platform2.y = y;
+    stage.addChild(platform2);
+}
+function tick()
+{
+    stage.update();
 }
